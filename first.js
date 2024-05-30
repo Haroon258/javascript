3bf74cd43e9d1e1cdbdf394a80de8eb1e5b6d961
Haroon258 <haroonrasheed1437@gmail.com>
2024-05-30 07:49:04 +0000

    Your commit message

--- first.js
+++ first.js
@@ -1,10 +1,65 @@
 // 1:console
-
 // console.log("hello mr harry");
+
+
 
 // 2:variable
 // name ="tonny stark"
 // console.log(name);
 
+
+
+
 // 3:let var const variables
-let age = "24";
+// let age = "24";
+// age='33';
+// console.log(age);
+// mostly we use let variable
+
+// {
+//     let a='2';
+//     console.log(a);
+// }
+// this is one block inside curly brases
+
+// {
+//        let a='20';
+//          console.log(a);
+//         }
+    // this is an other block inside curly brases
+
+
+
+
+
+
+    // 4:  data types
+//   ptimitive data types  are 7
+
+// nonprimitive data type
+
+
+
+
+
+
+
+// object
+// const student ={
+//     fullNmae :"harry",
+//     age:24,
+//     cgpa:3.8,
+
+// };
+// console.log(student.cgpa);
+// iiiiiiiiiiiiiiiiiiiiiimmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmppppppppppppppppppppppppp
+
+
+
+
+
+
+// lecture no 2
+
+
+

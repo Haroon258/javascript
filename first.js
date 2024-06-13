1eddab385e9460f92932cdec72d9f2ed8d06ea1f
Haroon258 <haroonrasheed1437@gmail.com>
2024-06-13 05:59:59 +0000

    .

--- first.js
+++ first.js
@@ -130,14 +130,26 @@
 // }
 
 
- let age = 70;
+// //  8:
+// // turnery operater ?
+// let age = 67;
+// let result=age>=18 ?  'adult' : 'not adult'; 
+// console.log(result);
 
-if (age>=18){
-    console.log("you can vote");
+
+
+let score = 33;
+let grade;
+if(score>=90 && score<=100){
+  grade="A";
+}else if (score>=80 && score<=89){
+grade="B";
+}else if (score>=70 && score<=79){
+grade="C";
+}else if (score>=60 && score<=69){
+    grade="D";
+}else if(score>=0 && score<=49){
+grade="F";
 }
-if (age<18){
-    console.log("you cant vote");
-}
-<Link href={href} className='nav-item font-medium' onClick={onClick}>
-        {children}
-    </Link>
+
+console.log(grade);
